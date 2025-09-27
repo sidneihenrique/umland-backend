dfacac198ccdccdbecd972f5170e24836ade02b7
Sidnei Henrique <henrisrs@hotmail.com>
2025-09-27 18:06:58 -0300

    Adicionado métodos para usar itens e adicionar itens ao inventário do usuário

--- umland/src/main/java/com/umland/dao/InventoryDao.java
+++ umland/src/main/java/com/umland/dao/InventoryDao.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InventoryDao extends JpaRepository<Inventory, Integer> {
+	Inventory findByUserId(Integer userId);
 }
